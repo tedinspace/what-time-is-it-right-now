35136cab705ecc60544c8386061bd55c37a3bcf0
tedinspace <theodore.g.rose@gmail.com>
2023-02-06 22:01:59 -0500

    update upper bar

--- src/components/util/TedHeader.tsx
+++ src/components/util/TedHeader.tsx
@@ -1,28 +1,36 @@
 import { Header, Box, Text } from "grommet";
-import { Deploy } from "grommet-icons";
+import { Deploy, Github, Home } from "grommet-icons";
 
-export const DARK1 = "#1C2127";
-export const DARK3 = "#2F343C"
-
-export const LIGHT0 = "#f0e5e1"
-export const POP1 = "#c96d4f"
-export const TED = "#a4c8de"
+ const DARK3 = "#2F343C"
+ const LIGHT0 = "#f0e5e1"
+ const TED = "#a4c8de"
 export function TedHeader() {
   return (
     <Header
       pad="small"
       background={DARK3}
       style={{
-        borderBottom: "2px solid " + DARK1,
+        borderBottom: "2px solid #404854" 
       }}
     >
       <Box direction="row">
-        <Deploy size="medium" color={POP1} />
+      <a href="http://tedsite.com">
+          <Deploy size="medium" color={LIGHT0} />
+        </a>
         &nbsp;&nbsp;
         <Text size="large">
           <b><span style={{color:TED}}>Ted</span>Site</b>
           <i>.com</i>
         </Text>
+      </Box>
+      <Box direction="row">
+        <a href="https://tedsite.com/">
+          <Home color={TED} />
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://github.com/tedinspace">
+          <Github color={TED}  />
+        </a>
       </Box>
     </Header>
   );
